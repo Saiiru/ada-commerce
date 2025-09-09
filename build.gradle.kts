66a439d731e05b8126020f71a7f12d7c4ebc40e2
[Sairu] <sairufsa@hotmail.com>
2025-09-09 16:42:47 -0300

    feat(domain): skeleton de entidades e VOs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,26 @@
 plugins {
-    java
+  application
+  java
 }
 
 group = "com.ada"
 version = "0.1.0"
 
 java {
-    toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
+  toolchain { languageVersion.set(JavaLanguageVersion.of(17)) }
 }
 
 repositories { mavenCentral() }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test { useJUnitPlatform() }
+
+// a classe Main precisa existir neste pacote
+application {
+  application { mainClass.set("com.ada.commerce.controllers.cli.Main") }
+}
+
---
+++ src/main/java/com/ada/commerce/controllers/cli/Main.java
@@ -0,0 +1,8 @@
+package com.ada.commerce.controllers.cli;
+
+public final class Main {
+  public static void main(String[] args) {
+    System.out.println("Ada Commerce CLI ok.");
+  }
+}
+
