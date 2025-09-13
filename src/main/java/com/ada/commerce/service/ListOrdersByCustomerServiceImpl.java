91ee013aa266aa365cedec65cdb118926de4d315
Carlucio Santos <carlucios@gmail.com>
2025-09-13 13:19:07 -0300

    Add ListOrdersByCustomerService and ListAllOrdersService implementations

---
+++ src/main/java/com/ada/commerce/service/ListOrdersByCustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ada.commerce.service;
+
+import com.ada.commerce.model.Order;
+import com.ada.commerce.repository.OrderRepository;
+
+import java.util.List;
+
+public class ListOrdersByCustomerServiceImpl implements ListOrdersByCustomerService {
+
+  private final OrderRepository repository;
+
+  public ListOrdersByCustomerServiceImpl(OrderRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public List<Order> execute(Customer owner) {
+    if (owner == null) {
+      throw new IllegalArgumentException("O cliente é um dado obrigatório");
+    }
+
+    return repository.findByCustomer(owner);
+  }
+}
